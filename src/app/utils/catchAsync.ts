e5c3a612eb4e3796e1b330a601887a128347ed40
Shakhawat <shakhawat2021hossain2021@gmail.com>
2025-07-30 23:42:29 +0600

    custom AppError handler extending Error, catchAsync implemented

---
+++ src/app/utils/catchAsync.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+
+export const catchAsync = (fn: RequestHandler) =>
+    (req: Request, res: Response, next: NextFunction) => {
+        Promise.resolve(fn(req, res, next)).catch((err) => {
+            // console.log(err);
+            next(err)
+        })
+    }
