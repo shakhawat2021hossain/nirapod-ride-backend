b526f9c87e10c5c7fc810706757614adb617965a
Shakhawat <shakhawat2021hossain2021@gmail.com>
2025-07-30 12:38:36 +0600

    created user interface, schema and model

--- src/app/modules/user/user.interface.ts
+++ src/app/modules/user/user.interface.ts
@@ -1,6 +1,37 @@
+export enum Role {
+    ADMIN = "ADMIN",
+    RIDER = "RIDER",
+    DRIVER = "DRIVER"
+}
+
+export enum Availability {
+    ACTIVE = "ACTIVE",
+    BLOCKED = "BLOCKED"
+}
+
+export interface IVehicle {
+    type: "car" | "cng" | "bike" | "auto";
+    model: string;
+    plateNum: string;
+    // passengerCapacity: number; 
+}
+
+export interface IAuthProvider{
+    provider: "credentials" | "google";
+    providerId: string
+}
 export interface IUser {
     name: string;
     email: string;
+    phone?: string;
     password?: string;
-    
+    role: Role;
+    picture?: string;
+    availability?: Availability;
+    isDeleted?: boolean;
+    auths?: IAuthProvider[];
+    address?: string;
+    isVerified?: boolean;
+    isOnline?: boolean;
+    behicleInfo?: IVehicle;
 }
---
+++ src/app/modules/user/user.model.ts
@@ -0,0 +1,42 @@
+import { model, Schema } from "mongoose";
+import { Availability, IAuthProvider, IUser, IVehicle, Role } from "./user.interface";
+const authProviderSchema = new Schema<IAuthProvider>({
+    provider: {type: String},
+    providerId: {type: String}
+},{
+    versionKey: false,
+    _id: false
+})
+const vehicleSchema = new Schema<IVehicle>({
+    model: {type: String, required: true},
+    type: {type: String, required: true},
+    plateNum: {type: String, required: true}
+},{
+    versionKey: false,
+    _id: false
+})
+const userSchema = new Schema<IUser>({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String },
+    role: {
+        type: String,
+        enum: Object.values(Role),
+        default: Role.RIDER
+    },
+    phone: { type: String },
+    picture: { type: String },
+    address: { type: String },
+    isDeleted: { type: Boolean, default: false },
+    isOnline: { type: Boolean, default: false },
+    behicleInfo: vehicleSchema,
+    availability: {
+        type: String,
+        enum: Object.values(Availability),
+        default: Availability.ACTIVE
+    },
+    isVerified: { type: Boolean, default: false },
+    auths: [authProviderSchema],
+})
+
+export const User = model("User", userSchema)
